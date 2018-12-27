da3105ba83cb68cf3c7a22e81cf3e8a448069e29
Lorena Pinheiro <lorenapinheiro@softbox.com.br>
2018-12-27 17:42:29 -0200

    Components and mock

---
+++ src/components/List/ListTree.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Accordion } from 'semantic-ui-react'
+
+class ListTree extends React.Component {
+  render() {
+    return (
+      <Accordion></Accordion>
+    );
+  }
+
+
+
+}
+
+
+export default ListTree;
